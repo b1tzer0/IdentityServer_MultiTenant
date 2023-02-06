2e8d7754206080591916853b4672ca6a7378b66f
Rick <b1t_zer0@hotmail.com>
2023-02-06 09:10:38 -0600

    Added Console App for generating encoded client secret.

---
+++ ClientSecretGenerator/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Duende.IdentityServer.Models;
+
+using IdentityModel;
+
+Console.Write("Enter your client secret: ");
+string clientSecret = Console.ReadLine().ToSha256();
+
+var sec = new Secret(clientSecret);
+Console.WriteLine($"Your encoded client secret is: {sec.Value}");
+
+Console.WriteLine("Press any key to exit.");
+Console.ReadKey();
